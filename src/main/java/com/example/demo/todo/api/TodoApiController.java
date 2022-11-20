89f687358ed14fb9f8dca0fa303029805532eae1
이도협 <guqeh1@gmail.com>
2022-11-20 14:02:18 +0900

    #2 jsonController

--- src/main/java/com/example/demo/todo/api/TodoApiController.java
+++ src/main/java/com/example/demo/todo/api/TodoApiController.java
@@ -1,7 +1,9 @@
 package com.example.demo.todo.api;
 
 import com.example.demo.todo.entity.ToDo;
+import com.example.demo.todo.repository.TodoRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,17 @@ import java.util.List;
 @RequestMapping("/api/todos")
 public class TodoApiController {
 
+    private TodoRepository repository;
+
+    @Autowired
+    public TodoApiController(TodoRepository repository) {
+        this.repository = repository;
+    }
+
     // 할 일 목록 전체조회 요청
     @GetMapping
     public List<ToDo> todos(){
         log.info("/api/todos GET request!");
+        return null;
     }
 }
